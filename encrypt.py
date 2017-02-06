07775c37c99f8553cdfa3b28f683909f21178dbb
Vlad Orlenko <orlenko@gmail.com>
2017-02-05 21:23:53 -0800

    Add netchat script

---
+++ encrypt.py
@@ -0,0 +1,7 @@
+
+def encrypt(text):
+    return text
+
+
+if __name__ == '__main__':
+    print 'Encrypting "foo":', encrypt("foo")
---
+++ simplest.py
@@ -0,0 +1,6 @@
+
+print 'Hello, this is a simple python script!'
+
+
+for i in range(10):
+    print 'We are inside a loop! This is iteration number', i
